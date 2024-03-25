d659ae383999f9ece18a56a62d3cb817c8844c0d
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-25 15:15:34 +0100

    update unit test

--- unit tests/database.php
+++ unit tests/database.php
@@ -2,3 +2,4 @@
 
 include_once("../php classes/database.php");
 $database = new Database("localhost", "todo_manager", "root", "");
+$database->create("accounts", ["first_name"=> "asd", "last_name"=>"asd", "password_hash"=>"asd"]);
