5b08c8232321c680fb639843e362599768271f14
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-25 15:14:57 +0100

    add create function

--- php classes/database.php
+++ php classes/database.php
@@ -18,4 +18,32 @@ class Database
             echo $e->getMessage();
         }
     }
+
+    //column_value_pairs is an associative array: [column => value]
+    public function create($table_name, $column_value_pairs)
+    {
+        $columns = array_keys($column_value_pairs);
+        $columns_imploded = implode(", ", $columns);
+        $placeholders_imploded = ":" . implode(", :", $columns);
+        $sql = "INSERT INTO $table_name ($columns_imploded) VALUES ($placeholders_imploded)";
+        $statement = $this->pdo->prepare($sql);
+
+        foreach ($column_value_pairs as $column => &$value) {
+            $statement->bindParam(':' . $column, $value);
+        }
+
+        $statement->execute();
+    }
+
+    public function read()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
 }
