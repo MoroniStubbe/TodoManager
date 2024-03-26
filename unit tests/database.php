dfa5c215fdfb4c1b1d1829d9e0955f77996408d7
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-26 09:58:23 +0100

    add unit test for read function

--- unit tests/database.php
+++ unit tests/database.php
@@ -2,4 +2,8 @@
 
 include_once("../php classes/database.php");
 $database = new Database("localhost", "todo_manager", "root", "");
-$database->create("accounts", ["first_name"=> "asd", "last_name"=>"asd", "password_hash"=>"asd"]);
+
+// $database->create("accounts", ["first_name"=> "asd", "last_name"=>"asd", "password_hash"=>"asd"]);
+
+$account = $database->read("accounts", ["first_name", "last_name"], ["id = 3"])[0];
+print($account["first_name"] . " " . $account["last_name"]);
