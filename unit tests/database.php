c24ac81129968e9f7f39ee060c4af5167df982ce
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-26 10:17:54 +0100

    update create() unit test

--- unit tests/database.php
+++ unit tests/database.php
@@ -3,7 +3,9 @@
 include_once("../php classes/database.php");
 $database = new Database("localhost", "todo_manager", "root", "");
 
+//Test create()
 // $database->create("accounts", ["first_name"=> "asd", "last_name"=>"asd", "password_hash"=>"asd"]);
 
-$account = $database->read("accounts", ["first_name", "last_name"], ["id = 3"])[0];
-print($account["first_name"] . " " . $account["last_name"]);
+//Test read()
+// $account = $database->read("accounts", ["first_name", "last_name"], ["id = 3"])[0];
+// print($account["first_name"] . " " . $account["last_name"]);
