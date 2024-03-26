b538bf55be7536e97dd4aa167b6bdaa3ffac048b
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-26 15:47:36 +0100

    update read() unit test

--- unit tests/database.php
+++ unit tests/database.php
@@ -6,6 +6,6 @@ $database = new Database("localhost", "todo_manager", "root", "");
 //Test create()
 // $database->create("accounts", ["first_name"=> "asd", "last_name"=>"asd", "password_hash"=>"asd"]);
 
-//Test read()
-// $account = $database->read("accounts", ["first_name", "last_name"], ["id = 3"])[0];
-// print($account["first_name"] . " " . $account["last_name"]);
+// Test read()
+// $accounts = $database->read("accounts", ["first_name", "last_name"], ["id" => 3]);
+// print($accounts[0]["first_name"] . " " . $accounts[0]["last_name"]);
