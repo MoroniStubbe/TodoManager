9d4d5ce55ac35258a81e43b1dfae1c201a2f0272
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-27 13:19:33 +0100

    add read()

--- php classes/account.php
+++ php classes/account.php
@@ -12,3 +12,22 @@ class Account
     {
         $this->database = $database;
     }
+
+    //returns true if account was found
+    public function read()
+    {
+        $account = [];
+
+        if ($this->id !== null) {
+            $account = $this->database->read($this->table, where: ["id" => $this->id])[0];
+            $this->username = $account["username"];
+            return true;
+        } else if ($this->username !== "") {
+            $account = $this->database->read($this->table, where: ["username" => $this->username])[0];
+            $this->id = $account["id"];
+            return true;
+        }
+
+        return false;
+    }
+}
