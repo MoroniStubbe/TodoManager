c157168e26434c937c7efee7ae6af8d9f17148cc
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-27 22:22:24 +0100

    add log_in() unit test

--- unit tests/account.php
+++ unit tests/account.php
@@ -5,5 +5,15 @@ include_once("../php classes/account.php");
 $account = new Account($database);
 $account->username = "asd";
 $account->password = "asd";
+
+//Test create()
 // $account->create();
+
+//Test read()
 // $account->read();
+
+//Test log_in()
+// $account->password = "asd";
+// $account->log_in();
+
+//$a = 1;
