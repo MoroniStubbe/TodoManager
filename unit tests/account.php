948b2c69558b9bc2cadef2d22811755dbb1dc726
MoroniStubbe <moronistubbe2000@gmail.com>
2024-03-28 23:07:37 +0100

    add delete() unit test

--- unit tests/account.php
+++ unit tests/account.php
@@ -3,23 +3,28 @@ include_once("database.php");
 include_once("../php classes/account.php");
 
 $account = new Account($database);
-$account->set_username("asd");
-$account->set_password("asdasdasdasd");
+// $account->set_username("asd");
+// $account->set_password("asdasdasdasd");
 
 //Test create()
-$account->create();
+// $account->create();
 
 //Test read()
 $account->read(username: "asd");
 
 //Test update()
-$account->set_username("qwe");
-$account->set_password("qweqweqweqwe");
-$account->update();
+// $account->set_username("qwe");
+// $account->set_password("qweqweqweqwe");
+// $account->update();
 
-//Test log_in() and log_out()
-$account->log_in("asd"); //wrong password
+//Test log_in()
+// $account->log_in("asd"); //wrong password
 $account->log_in("qweqweqweqwe"); //correct password
-$account->log_out();
+
+//Test delete()
+$account->delete();
+
+//Test log_out()
+// $account->log_out();
 
 $a = 1; //to break here to read the var above
