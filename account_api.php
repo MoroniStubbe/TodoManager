d7e795ba92f8f735cd7a9eb5ae14035445ad6c60
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-02 10:19:15 +0200

    add log in

--- account_api.php
+++ account_api.php
@@ -3,12 +3,15 @@
 //TODO: replace $_GET for $_POST
 require_once("php classes/database.php");
 $database = new Database("localhost", "todo_manager", "root", "");
+require_once("php classes/account.php");
+$account = new Account($database);
 
 switch ($_GET["action"]) {
     case "create":
-        include_once("php classes/account.php");
-        $account = new Account($database);
         $account->set_username($_GET["username"]);
         $account->set_password($_GET["password"]);
         $account->create();
+
+    case "log_in":
+        $account->log_in($_GET["password"]);
 }
