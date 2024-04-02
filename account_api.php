c62a70032af9132f0bc00ebd3a5050e11b6953fd
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-02 15:30:12 +0200

    add log out

--- account_api.php
+++ account_api.php
@@ -2,21 +2,21 @@
 
 //for now we use $_GET instead of $_POST just to test the functions
 //TODO: replace $_GET for $_POST
-if(isset($_GET["action"])){
+if (isset($_GET["action"])) {
     require_once("php classes/database.php");
     require_once("php classes/account.php");
-    
+
     session_start();
     $database = new Database("localhost", "todo_manager", "root", "");
     $account = new Account($database);
-    
+
     if (isset($_SESSION["account"])) {
         $account->import($_SESSION["account"]);
     }
-    
+
     switch ($_GET["action"]) {
         case "create":
-            if(isset($_GET["username"]) and isset($_GET["password"])){
+            if (isset($_GET["username"]) and isset($_GET["password"])) {
                 $account->set_username($_GET["username"]);
                 $account->set_password($_GET["password"]);
                 $account->create();
@@ -24,20 +24,24 @@ if(isset($_GET["action"])){
 
             $_SESSION["account"] = $account->export();
             break;
-    
+
         case "log_in":
-            if (isset($_GET["username"]) and isset($_GET["password"])){
+            if (isset($_GET["username"]) and isset($_GET["password"])) {
                 $account->set_username($_GET["username"]);
                 $account->log_in($_GET["password"]);
             }
 
             $_SESSION["account"] = $account->export();
             break;
-    
+
+        case "log_out":
+            $account->log_out();
+            session_destroy();
+            break;
+
         case "delete":
             $account->delete();
             session_destroy();
             break;
     }
-    
 }
