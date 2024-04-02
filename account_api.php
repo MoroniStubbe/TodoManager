cbfd69831515d48cd371d67c63f7406e3b8d0d69
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-02 14:03:23 +0200

    add session

--- account_api.php
+++ account_api.php
@@ -1,11 +1,17 @@
 <?php
-//for now we use $_GET instead of $_POST just to test the functions
-//TODO: replace $_GET for $_POST
 require_once("php classes/database.php");
-$database = new Database("localhost", "todo_manager", "root", "");
 require_once("php classes/account.php");
+
+session_start();
+$database = new Database("localhost", "todo_manager", "root", "");
 $account = new Account($database);
 
+if (isset($_SESSION["account"])) {
+    $account->import($_SESSION["account"]);
+}
+
+//for now we use $_GET instead of $_POST just to test the functions
+//TODO: replace $_GET for $_POST
 switch ($_GET["action"]) {
     case "create":
         $account->set_username($_GET["username"]);
@@ -15,3 +21,5 @@ switch ($_GET["action"]) {
     case "log_in":
         $account->log_in($_GET["password"]);
 }
+
+$_SESSION["account"] = $account->export();
