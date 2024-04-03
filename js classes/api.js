e2d79aefc5cfde4a28cac313f4f7c650198047c0
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-03 12:22:23 +0200

    add send()

--- js classes/api.js
+++ js classes/api.js
@@ -1,9 +1,23 @@
-class API{
+class API {
     url;
-    
+    method;
 
-    constructor(url){
+    constructor(url, method = "GET") {
         this.url = url;
+        this.method = method;
     }
 
+    async send(data, method = this.method) {
+        const options = {
+            method,
+            headers: { 'Content-Type': 'application/json' }
+        };
+
+        if (data) {
+            options.body = JSON.stringify(data);
+        }
+
+        const response = await fetch(this.url, options);
+        return await response.json();
+    }
 }
