d8448a52642e977d768f80506070f217cc91c4b8
AnticliMax8 <max.hal88@gmail.com>
2024-04-04 12:26:51 +0200

    html

--- index.php
+++ index.php
@@ -4,52 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checklist</title>
-    <style>
-        nav {
-            background-color: blue;
-            overflow: hidden;
-        }
-
-        nav a {
-            float: left;
-            display: block;
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-        }
-
-        nav a:hover {
-            background-color: red;
-            color: black;
-        }
-
-        /* Stijl voor rechts uitgelijnde links */
-        .right-align {
-            float: right;
-        }
-
-        .checked {
-            background-color: #c7e6c7;
-            color: #4CAF50;
-            text-decoration: line-through;
-        }
-
-        .checklist-item {
-            list-style-type: none;
-            margin: 10px 0;
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .test {
-            background-color: blue
-            hight: 110px;
-            wi
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <nav>
@@ -61,17 +16,50 @@
         </div>
     </nav>
 
-    <h1>Mijn Checklist</h1>
+    <div class="lijst">    
+    <h1 class="titellijst">Mijn Checklist</h1>
 
     <!-- Checklist -->
-    <ul id="checklist">
+    <ul class="checklist">
+        <li class="checklist-item" onclick="toggleCheck(this)">pak de laptop</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">open de laptop</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">druk op de aanknop</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">kijk of de laptop opstart</li>
+    </ul>
+</div>
+<div class="lijst">    
+    <h1 class="titellijst">Mijn Checklist</h1>
+
+    <!-- Checklist -->
+    <ul class="checklist">
+        <li class="checklist-item" onclick="toggleCheck(this)">punt 1</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 2</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 3</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 4</li>
+    </ul>
+</div>
+<div class="lijst">    
+    <h1 class="titellijst">Mijn Checklist</h1>
+
+    <!-- Checklist -->
+    <ul class="checklist">
         <li class="checklist-item" onclick="toggleCheck(this)">Punt 1</li>
         <li class="checklist-item" onclick="toggleCheck(this)">Punt 2</li>
         <li class="checklist-item" onclick="toggleCheck(this)">Punt 3</li>
         <li class="checklist-item" onclick="toggleCheck(this)">Punt 4</li>
     </ul>
+</div>
+<div class="lijst">    
+    <h1 class="titellijst">Mijn Checklist</h1>
 
-    <div class=test>
+    <!-- Checklist -->
+    <ul class="checklist">
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 1</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 2</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 3</li>
+        <li class="checklist-item" onclick="toggleCheck(this)">Punt 4</li>
+    </ul>
+</div>
 
     </div>
 
