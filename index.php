7c26ef270ed9c3b4afdf25199404b8cfcc85f1df
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-06 17:18:56 +0200

    move divs into main

--- index.php
+++ index.php
@@ -20,26 +20,28 @@
             <a href="">Nieuwe Lijst</a>
         </div>
     </nav>
-    <div id="about">
-        <h1>Over TodoManager</h1>
-        <p>
-            TodoManager is een handige tool waarmee je je eigen todo lijsten kunt maken en delen met anderen.
-        </p>
-    </div>
-    <div id="todo-list">
-        <h1 id="todo-list-title">$todo_lists[$currentTodo->id]->title</h1>
-        <ul id="todos">
-            <li class="todo" onclick="toggleCheck(this)">$todos[0]->text</li>
-            <li class="todo" onclick="toggleCheck(this)">$todos[1]->text</li>
-        </ul>
-    </div>
-    <div id="todo-lists">
-        <h1>Mijn todo lijsten</h1>
-        <ul>
-            <li id="todo-list-1"><a href="">$todo_lists[0]->title</a></li>
-            <li id="todo-list-2"><a href="">$todo_lists[1]->title</a></li>
-        </ul>
-    </div>
+    <main>
+        <div id="about">
+            <h1>Over TodoManager</h1>
+            <p>
+                TodoManager is een handige tool waarmee je je eigen todo lijsten kunt maken en delen met anderen.
+            </p>
+        </div>
+        <div id="todo-list">
+            <h1 id="todo-list-title">$todo_lists[$currentTodo->id]->title</h1>
+            <ul id="todos">
+                <li class="todo" onclick="toggleCheck(this)">$todos[0]->text</li>
+                <li class="todo" onclick="toggleCheck(this)">$todos[1]->text</li>
+            </ul>
+        </div>
+        <div id="todo-lists">
+            <h1>Mijn todo lijsten</h1>
+            <ul>
+                <li id="todo-list-1"><a href="">$todo_lists[0]->title</a></li>
+                <li id="todo-list-2"><a href="">$todo_lists[1]->title</a></li>
+            </ul>
+        </div>
+    </main>
 </body>
 
 </html>
