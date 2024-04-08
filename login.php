3001304c4f7cb4b159a06c359c7533fe56d5b717
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-08 17:34:47 +0200

    fix can visit login.php when already logged in

--- login.php
+++ login.php
@@ -1,2 +1,15 @@
 <?php
-echo file_get_contents("html templates/login.html");
+require_once("php classes/database.php");
+require_once("php classes/account.php");
+
+$database = new Database("localhost", "todo_manager", "root", "");
+$account = new Account($database);
+session_start();
+
+if (isset($_SESSION["account"])) {
+    $account->import($_SESSION["account"]);
+}
+
+if (!$account->is_logged_in()) {
+    echo file_get_contents("html templates/login.html");
+}
