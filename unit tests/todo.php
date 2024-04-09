fb4b955c561b3ae06ce4859f478a9c2c759853f0
Aidan De Groot <aidanjdegroot@gmail.com>
2024-04-09 15:23:40 +0200

    add crud unit test

--- unit tests/todo.php
+++ unit tests/todo.php
@@ -4,10 +4,12 @@ require_once ("database.php");
 
 require_once ("../php classes/todo.php");
 
-$database = new Database("localhost", "todo_manager", "root", "root");
+$database = new Database("localhost", "todo_manager", "root", "");
 
 $todo = new Todo($database);
 
 $todo->text = "testText";
+
+
 
 $todo->create();
